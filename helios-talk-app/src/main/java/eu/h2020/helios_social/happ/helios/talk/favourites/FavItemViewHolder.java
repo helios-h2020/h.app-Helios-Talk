f11c1e22d889230440ef5516c183c22916f260d1
vasgat <vasgat@iti.gr>
2021-03-11 18:16:11 +0200

    Modify FavItemViewHolder to support the preview of image attachments

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavItemViewHolder.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavItemViewHolder.java
@@ -2,59 +2,87 @@ package eu.h2020.helios_social.happ.helios.talk.favourites;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.CallSuper;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.recyclerview.widget.RecyclerView;
-import de.hdodenhof.circleimageview.CircleImageView;
+
 import eu.h2020.helios_social.happ.helios.talk.R;
+import eu.h2020.helios_social.happ.helios.talk.conversation.ConversationListener;
+import eu.h2020.helios_social.happ.helios.talk.conversation.ImageAdapter;
+import eu.h2020.helios_social.happ.helios.talk.conversation.ImageItemDecoration;
+import eu.h2020.helios_social.happ.helios.talk.group.GroupMessageItem;
 import eu.h2020.helios_social.happ.helios.talk.util.UiUtils;
+import eu.h2020.helios_social.happ.helios.talk.view.AuthorView;
 
 
 public class FavItemViewHolder extends RecyclerView.ViewHolder {
 
-	protected final ConstraintLayout layout;
-	protected final CircleImageView avatar;
-	protected final TextView message;
-	protected final TextView name;
-	@Nullable
-	private final TextView date;
-	private final ConstraintSet textConstraints = new ConstraintSet();
-	private final ConstraintSet imageConstraints = new ConstraintSet();
-	private final ConstraintSet imageTextConstraints = new ConstraintSet();
+    private final ImageAdapter adapter;
+    protected final ConstraintLayout layout;
+    protected final AuthorView author;
+    protected final TextView text;
+    protected final ImageView favourite;
+    @Nullable
+    private String itemKey = null;
+    private RecyclerView list;
+    private final ConstraintSet layoutConstraints = new ConstraintSet();
+    private final LinearLayout attachmentsPreview;
 
-	public FavItemViewHolder(View v) {
-		super(v);
+    public FavItemViewHolder(View v, RecyclerView.RecycledViewPool imageViewPool,
+                             ImageItemDecoration imageItemDecoration) {
+        super(v);
+        layout = v.findViewById(R.id.layout);
+        text = v.findViewById(R.id.text);
+        author = v.findViewById(R.id.author);
+        favourite = v.findViewById(R.id.favourite);
+        attachmentsPreview = v.findViewById(R.id.attachmentsPreview);
 
-		layout = v.findViewById(R.id.layout);
-		avatar = v.findViewById(R.id.avatar);
-		name = v.findViewById(R.id.authorName);
-		date = v.findViewById(R.id.dateView);
-		message = v.findViewById(R.id.text);
+        // image list
+        list = v.findViewById(R.id.imageList);
+        list.setRecycledViewPool(imageViewPool);
+        layoutConstraints.clone(v.getContext(),
+                R.layout.list_item_group_conversation_msg);
+        adapter = new ImageAdapter(v.getContext(), null);
+        list.setAdapter(adapter);
+        list.addItemDecoration(imageItemDecoration);
+    }
 
-		textConstraints.clone(v.getContext(),
-				R.layout.list_item_fav_content);
-		imageConstraints.clone(v.getContext(),
-				R.layout.list_item_fav_image);
-		imageTextConstraints.clone(v.getContext(),
-				R.layout.list_item_fav_image_text);
-	}
+    @CallSuper
+    void bind(Context ctx, FavItem item) {
+        itemKey = item.getKey();
+        author.setAuthor(item.getAuthor());
+        long timestamp = item.getTime();
+        author.setDate(timestamp);
 
-	protected void bind(Context ctx, FavItem item) {
-		avatar.setImageResource(R.drawable.ic_person);
-		name.setText(item.getAuthor());
-		long timestamp = item.getTime();
-		date.setText(UiUtils.formatDate(date.getContext(), timestamp));
-		message.setText(item.getText());
+        if (item.getAttachmentList().size() > 0) {
+            if (item.getText() == null) {
+                text.setVisibility(View.GONE);
+            } else {
+                text.setText(item.getText());
+                text.setVisibility(View.VISIBLE);
+            }
 
-		bindTextItem();
+            adapter.setConversationItem(item);
+            layoutConstraints.applyTo(layout);
+            attachmentsPreview.setVisibility(View.VISIBLE);
+        } else {
+            text.setText(item.getText());
+            attachmentsPreview.setVisibility(View.GONE);
+            adapter.clear();
+            layoutConstraints.applyTo(layout);
+        }
 
-	}
+    }
 
-	private void bindTextItem() {
-		textConstraints.applyTo(layout);
-	}
+    @Nullable
+    String getItemKey() {
+        return itemKey;
+    }
 
 }
