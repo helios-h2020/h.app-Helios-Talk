f079e6f08fcaae55121491349a5e563ace8e55d1
vasgat <vasgat@iti.gr>
2021-03-11 21:43:25 +0200

    Update TagEditText to use comma as separator between tags and add getTags method

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/profile/TagEditText.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/profile/TagEditText.java
@@ -16,168 +16,192 @@ import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import eu.h2020.helios_social.happ.helios.talk.R;
 
 @SuppressLint("AppCompatCustomView")
 public class TagEditText extends EditText {
 
-	TextWatcher textWatcher;
+    TextWatcher textWatcher;
 
-	String lastString;
+    String lastString;
 
-	String separator = " ";
+    String separator = ",";
 
-	public TagEditText(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
-
-
-	private void init() {
-		setMovementMethod(LinkMovementMethod.getInstance());
-
-		textWatcher = new TextWatcher() {
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-
-			}
-
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-				String thisString = s.toString();
-				if (thisString.length() > 0 && !thisString.equals(lastString)) {
-					format();
-
-				}
-			}
-		};
-
-		addTextChangedListener(textWatcher);
-	}
+    public TagEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
 
-	public void format() {
+    private void init() {
+        setMovementMethod(LinkMovementMethod.getInstance());
 
-		SpannableStringBuilder sb = new SpannableStringBuilder();
-		String fullString = getText().toString();
+        textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
 
-		String[] strings = fullString.split(separator);
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
 
-		for (int i = 0; i < strings.length; i++) {
+            }
 
-			String string = strings[i];
-			sb.append(string);
+            @Override
+            public void afterTextChanged(Editable s) {
+                String thisString = s.toString();
+                if (thisString.length() > 0 && !thisString.equals(lastString)) {
+                    format();
 
-			if (fullString.charAt(fullString.length() - 1) !=
-					separator.charAt(0) && i == strings.length - 1) {
-				break;
-			}
+                }
+            }
+        };
 
-			BitmapDrawable bd = (BitmapDrawable) convertViewToDrawable(
-					createTokenView(string));
-			bd.setBounds(0, 0, bd.getIntrinsicWidth(), bd.getIntrinsicHeight());
+        addTextChangedListener(textWatcher);
 
-			int startIdx = sb.length() - (string.length());
-			int endIdx = sb.length();
-
-			sb.setSpan(new ImageSpan(bd), startIdx, endIdx,
-					Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-			MyClickableSpan myClickableSpan =
-					new MyClickableSpan(startIdx, endIdx);
-			sb.setSpan(myClickableSpan, Math.max(endIdx - 2, startIdx), endIdx,
-					Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-			if (i < strings.length - 1) {
-				sb.append(separator);
-			} else if (fullString.charAt(fullString.length() - 1) ==
-					separator.charAt(0)) {
-				sb.append(separator);
-			}
-		}
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && !getText().toString().endsWith(",") && !getText().toString().equals("")) {
+                    setText(getText().toString() + ",");
+                    format();
+                }
+            }
+        });
+    }
 
 
-		lastString = sb.toString();
+    public void format() {
 
-		setText(sb);
-		setSelection(sb.length());
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        String fullString = getText().toString();
 
-	}
-
-	public View createTokenView(String text) {
-		LinearLayout l = new LinearLayout(getContext());
-		l.setOrientation(LinearLayout.HORIZONTAL);
-		l.setBackgroundResource(R.drawable.tag_bubble);
-
-		TextView tv = new TextView(getContext());
-		l.addView(tv);
-		tv.setText(text);
-		tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
-
-		ImageView im = new ImageView(getContext());
-		l.addView(im);
-		im.setImageResource(R.drawable.ic_close);
-		im.setScaleType(ImageView.ScaleType.FIT_CENTER);
-
-		return l;
-	}
-
-	public Object convertViewToDrawable(View view) {
-		int spec = MeasureSpec
-				.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
-		view.measure(spec, spec);
-		view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
-
-		Bitmap b = Bitmap.createBitmap(view.getMeasuredWidth(),
-				view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-		Canvas c = new Canvas(b);
-
-		c.translate(-view.getScrollX(), -view.getScrollY());
-		view.draw(c);
-		view.setDrawingCacheEnabled(true);
-		Bitmap cacheBmp = view.getDrawingCache();
-		Bitmap viewBmp = cacheBmp.copy(Bitmap.Config.ARGB_8888, true);
-		view.destroyDrawingCache();
-		return new BitmapDrawable(getContext().getResources(), viewBmp);
-	}
-
-	private class MyClickableSpan extends ClickableSpan {
-
-		int startIdx;
-		int endIdx;
-
-		public MyClickableSpan(int startIdx, int endIdx) {
-			super();
-			this.startIdx = startIdx;
-			this.endIdx = endIdx;
-		}
-
-		@Override
-		public void onClick(View widget) {
+        String[] strings = fullString.split(separator);
 
 
-			String s = getText().toString();
+        for (int i = 0; i < strings.length; i++) {
 
-			String s1 = s.substring(0, startIdx);
-			String s2 = s.substring(Math.min(endIdx + 1, s.length() - 1),
-					s.length());
+            String string = strings[i].trim();
+            sb.append(string);
 
-			TagEditText.this.setText(s1 + s2);
-		}
+            if (fullString.charAt(fullString.length() - 1) !=
+                    separator.charAt(0) && i == strings.length - 1) {
+                break;
+            }
 
-	}
+            BitmapDrawable bd = (BitmapDrawable) convertViewToDrawable(
+                    createTokenView(string));
+            bd.setBounds(0, 0, bd.getIntrinsicWidth(), bd.getIntrinsicHeight());
+
+            int startIdx = sb.length() - (string.length());
+            int endIdx = sb.length();
+
+            sb.setSpan(new ImageSpan(bd), startIdx, endIdx,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            MyClickableSpan myClickableSpan =
+                    new MyClickableSpan(startIdx, endIdx);
+            sb.setSpan(myClickableSpan, Math.max(endIdx - 2, startIdx), endIdx,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            if (i < strings.length - 1) {
+                sb.append(separator);
+            } else if (fullString.charAt(fullString.length() - 1) ==
+                    separator.charAt(0)) {
+                sb.append(separator);
+            }
+        }
+
+
+        lastString = sb.toString();
+
+        setText(sb);
+        setSelection(sb.length());
+
+    }
+
+    public View createTokenView(String text) {
+        LinearLayout l = new LinearLayout(getContext());
+        l.setOrientation(LinearLayout.HORIZONTAL);
+        l.setBackgroundResource(R.drawable.tag_bubble);
+
+        TextView tv = new TextView(getContext());
+        l.addView(tv);
+        tv.setText(text);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
+
+        ImageView im = new ImageView(getContext());
+        l.addView(im);
+        im.setImageResource(R.drawable.ic_close);
+        im.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        return l;
+    }
+
+    public Object convertViewToDrawable(View view) {
+        int spec = MeasureSpec
+                .makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        view.measure(spec, spec);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+        Bitmap b = Bitmap.createBitmap(view.getMeasuredWidth(),
+                view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas c = new Canvas(b);
+
+        c.translate(-view.getScrollX(), -view.getScrollY());
+        view.draw(c);
+        view.setDrawingCacheEnabled(true);
+        Bitmap cacheBmp = view.getDrawingCache();
+        Bitmap viewBmp = cacheBmp.copy(Bitmap.Config.ARGB_8888, true);
+        view.destroyDrawingCache();
+        return new BitmapDrawable(getContext().getResources(), viewBmp);
+    }
+
+    private class MyClickableSpan extends ClickableSpan {
+
+        int startIdx;
+        int endIdx;
+
+        public MyClickableSpan(int startIdx, int endIdx) {
+            super();
+            this.startIdx = startIdx;
+            this.endIdx = endIdx;
+        }
+
+        @Override
+        public void onClick(View widget) {
+
+
+            String s = getText().toString();
+
+            String s1 = s.substring(0, startIdx);
+            String s2 = s.substring(Math.min(endIdx + 1, s.length() - 1),
+                    s.length());
+
+            TagEditText.this.setText(s1 + s2);
+        }
+
+    }
+
+    public ArrayList<String> getTags() {
+        if (getText().toString().isEmpty()) return new ArrayList();
+        String[] array = getText().toString().split(separator);
+        ArrayList<String> tags = new ArrayList<>();
+        Collections.addAll(tags, array);
+        return tags;
+    }
 }
