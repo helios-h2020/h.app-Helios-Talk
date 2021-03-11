1d5d57553bc83dc6434bf5cab7738b94c3e2d331
vasgat <vasgat@iti.gr>
2021-03-11 18:15:16 +0200

    Modify FavItem to extend ConversationItem

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavItem.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavItem.java
@@ -1,43 +1,48 @@
 package eu.h2020.helios_social.happ.helios.talk.favourites;
 
 
+import androidx.annotation.LayoutRes;
+
+import java.util.List;
+
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
 
-import eu.h2020.helios_social.happ.helios.talk.api.nullsafety.NotNullByDefault;
+import eu.h2020.helios_social.happ.helios.talk.attachment.AttachmentItem;
+import eu.h2020.helios_social.happ.helios.talk.conversation.ConversationItem;
+import eu.h2020.helios_social.modules.groupcommunications.api.messaging.MessageHeader;
+import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.NotNullByDefault;
+
+import static eu.h2020.helios_social.modules.groupcommunications_utils.util.StringUtils.toHexString;
 
 
 @NotThreadSafe
 @NotNullByDefault
-public class FavItem {
+public class FavItem extends ConversationItem {
 
-	@Nullable
-	private String text;
-	private final String messageId;
-	private String author;
-	private final long time;
+    private String author;
 
-	FavItem(String messageId, String author, long timestamp, String text) {
-		this.messageId = messageId;
-		this.author = author;
-		this.time = timestamp;
-		this.text = text;
-	}
+    public FavItem(int layoutRes, String author, String text, String messageId, long time) {
+        super(layoutRes, text, messageId, time);
+        this.author = author;
+    }
 
-	@Nullable
-	public String getText() {
-		return text;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	public String getMessageId() {
-		return messageId;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public long getTime() {
-		return time;
-	}
+    @Override
+    public String toString() {
+        return "FavItem{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                ", messageId='" + messageId + '\'' +
+                ", groupId='" + groupId + '\'' +
+                ", time=" + time +
+                ", isIncoming=" + isIncoming +
+                ", favourite=" + favourite +
+                ", state=" + state +
+                ", list=" + list +
+                '}';
+    }
 }
