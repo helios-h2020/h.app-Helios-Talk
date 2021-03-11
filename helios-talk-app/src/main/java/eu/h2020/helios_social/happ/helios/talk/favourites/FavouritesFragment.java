b3e0d42decdfb67b2d281fffcd7c447a9bc7a41c
vasgat <vasgat@iti.gr>
2021-03-11 18:18:10 +0200

    Modify FavouritesFragment to support image preview attachments of messages marked as favourites

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavouritesFragment.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/favourites/FavouritesFragment.java
@@ -1,5 +1,6 @@
 package eu.h2020.helios_social.happ.helios.talk.favourites;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,127 +17,206 @@ import javax.inject.Inject;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
 import eu.h2020.helios_social.core.contextualegonetwork.ContextualEgoNetwork;
 import eu.h2020.helios_social.happ.helios.talk.R;
 import eu.h2020.helios_social.happ.helios.talk.activity.ActivityComponent;
-import eu.h2020.helios_social.happ.helios.talk.api.nullsafety.MethodsNotNullByDefault;
-import eu.h2020.helios_social.happ.helios.talk.api.nullsafety.ParametersNotNullByDefault;
+import eu.h2020.helios_social.happ.helios.talk.attachment.AttachmentItem;
+import eu.h2020.helios_social.happ.helios.talk.attachment.AttachmentRetriever;
+import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Message;
+import eu.h2020.helios_social.modules.groupcommunications_utils.Pair;
+import eu.h2020.helios_social.modules.groupcommunications_utils.contact.event.PendingContactAddedEvent;
+import eu.h2020.helios_social.modules.groupcommunications_utils.context.ContextInvitationAddedEvent;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.Event;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventListener;
+import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.MethodsNotNullByDefault;
+import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.ParametersNotNullByDefault;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.GroupInvitationAddedEvent;
 import eu.h2020.helios_social.happ.helios.talk.fragment.HeliosContextFragment;
 import eu.h2020.helios_social.happ.helios.talk.view.HeliosTalkRecyclerView;
+import eu.h2020.helios_social.modules.groupcommunications.api.contact.PendingContactType;
 import eu.h2020.helios_social.modules.groupcommunications.api.exception.DbException;
 import eu.h2020.helios_social.modules.groupcommunications.api.exception.FormatException;
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Favourite;
 import eu.h2020.helios_social.modules.groupcommunications.api.conversation.ConversationManager;
 
-import static eu.h2020.helios_social.happ.helios.talk.api.util.LogUtils.logDuration;
-import static eu.h2020.helios_social.happ.helios.talk.api.util.LogUtils.logException;
-import static eu.h2020.helios_social.happ.helios.talk.api.util.LogUtils.now;
+import static eu.h2020.helios_social.modules.groupcommunications_utils.util.LogUtils.logDuration;
+import static eu.h2020.helios_social.modules.groupcommunications_utils.util.LogUtils.logException;
+import static eu.h2020.helios_social.modules.groupcommunications_utils.util.LogUtils.now;
+import static java.util.Collections.emptyList;
 import static java.util.logging.Level.WARNING;
 
 @MethodsNotNullByDefault
 @ParametersNotNullByDefault
-public class FavouritesFragment extends HeliosContextFragment {
+public class FavouritesFragment extends HeliosContextFragment implements EventListener {
 
-	public static final String TAG = FavouritesFragment.class.getName();
-	private static final Logger LOG = Logger.getLogger(TAG);
+    public static final String TAG = FavouritesFragment.class.getName();
+    private static final Logger LOG = Logger.getLogger(TAG);
 
-	//private ChatListAdapter adapter;
-	private HeliosTalkRecyclerView list;
-	private FavListAdapter adapter;
+    //private ChatListAdapter adapter;
+    private HeliosTalkRecyclerView list;
+    private FavListAdapter adapter;
+    private FavouritesViewModel viewModel;
+    private AttachmentRetriever attachmentRetriever;
 
-	@Inject
-	volatile ConversationManager conversationManager;
-	@Inject
-	volatile ContextualEgoNetwork egoNetwork;
+    @Inject
+    volatile ConversationManager conversationManager;
+    @Inject
+    volatile ContextualEgoNetwork egoNetwork;
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
 
 
-	public static FavouritesFragment newInstance() {
-		Bundle args = new Bundle();
-		FavouritesFragment fragment = new FavouritesFragment();
-		fragment.setArguments(args);
-		return fragment;
-	}
+    public static FavouritesFragment newInstance() {
+        Bundle args = new Bundle();
+        FavouritesFragment fragment = new FavouritesFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-	@Override
-	public String getUniqueTag() {
-		return TAG;
-	}
+    @Override
+    public String getUniqueTag() {
+        return TAG;
+    }
 
-	@Override
-	public void injectFragment(ActivityComponent component) {
-		component.inject(this);
-	}
+    @Override
+    public void injectFragment(ActivityComponent component) {
+        component.inject(this);
+    }
 
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container,
-			@Nullable Bundle savedInstanceState) {
-		super.onCreateView(inflater, container, savedInstanceState);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
 
-		View contentView = inflater.inflate(R.layout.list,
-				container, false);
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(FavouritesViewModel.class);
+        attachmentRetriever = viewModel.getAttachmentRetriever();
 
-		adapter = new FavListAdapter(requireContext());
-		list = contentView.findViewById(R.id.list);
-		list.setLayoutManager(new LinearLayoutManager(requireContext()));
-		list.setAdapter(adapter);
-		list.setEmptyImage(R.drawable.ic_star_disable);
-		list.setEmptyText(getString(R.string.no_favourites));
+        View contentView = inflater.inflate(R.layout.list,
+                container, false);
 
-		return contentView;
-	}
+        adapter = new FavListAdapter(requireContext());
+        list = contentView.findViewById(R.id.list);
+        list.setLayoutManager(new LinearLayoutManager(requireContext()));
+        list.setAdapter(adapter);
+        list.setEmptyImage(R.drawable.ic_star_disable);
+        list.setEmptyText(getString(R.string.no_favourites));
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		loadFavourites();
-		list.startPeriodicUpdate();
-	}
+        return contentView;
+    }
 
-	@Override
-	public void onStop() {
-		super.onStop();
-		list.showProgressBar();
-		list.stopPeriodicUpdate();
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        loadFavourites();
+        list.startPeriodicUpdate();
+    }
 
-	private void loadFavourites() {
-		int revision = adapter.getRevision();
-		listener.runOnDbThread(() -> {
-			try {
-				long start = now();
-				List<FavItem> favs = new ArrayList<>();
+    @SuppressLint("RestrictedApi")
+    public void onResume() {
+        super.onResume();
+        actionBar.invalidateOptionsMenu();
+    }
 
-				String context =
-						egoNetwork.getCurrentContext().getData().toString()
-								.split("%")[1];
-				Collection<Favourite> favourites;
-				favourites = conversationManager.getFavourites(context);
-				for (Favourite fav : favourites) {
-					FavItem item =
-							new FavItem(fav.getMessageId(), fav.getAuthor(),
-									fav.getTimestamp(), fav.getTextMessage());
-					favs.add(item);
-				}
-				logDuration(LOG, "Favourites Full load", start);
-				displayFavourites(revision, favs);
-			} catch (DbException | FormatException e) {
-				logException(LOG, WARNING, e);
-			}
-		});
-	}
+    @Override
+    public void onStop() {
+        super.onStop();
+        list.showProgressBar();
+        list.stopPeriodicUpdate();
+    }
 
-	private void displayFavourites(int revision, List<FavItem> favs) {
-		runOnUiThreadUnlessDestroyed(() -> {
-			if (revision == adapter.getRevision()) {
-				adapter.incrementRevision();
-				if (favs.isEmpty()) list.showData();
-				else adapter.replaceAll(favs);
-			} else {
-				loadFavourites();
-			}
-		});
-	}
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void eventOccurred(Event e) {
+        if (e instanceof ContextInvitationAddedEvent) {
+            if (((ContextInvitationAddedEvent) e).getInvite().isIncoming())
+                actionBar.invalidateOptionsMenu();
+        } else if (e instanceof GroupInvitationAddedEvent) {
+            if (((GroupInvitationAddedEvent) e).getInvite().isIncoming())
+                actionBar.invalidateOptionsMenu();
+        } else if (e instanceof PendingContactAddedEvent) {
+            if (((PendingContactAddedEvent) e).getPendingContact().getPendingContactType().equals(PendingContactType.INCOMING))
+                actionBar.invalidateOptionsMenu();
+        }
+    }
+
+    private void loadFavourites() {
+        int revision = adapter.getRevision();
+        listener.runOnDbThread(() -> {
+            try {
+                long start = now();
+                List<FavItem> favs = new ArrayList<>();
+
+                String context =
+                        egoNetwork.getCurrentContext().getData().toString()
+                                .split("%")[1];
+                Collection<Favourite> favourites;
+                favourites = conversationManager.getFavourites(context);
+                for (Favourite fav : favourites) {
+                    FavItem item =
+                            new FavItem(R.layout.list_item_fav, fav.getAuthor(), fav.getTextMessage(), fav.getMessageId(),
+                                    fav.getTimestamp());
+                    if (fav.getType() == Message.Type.IMAGES) {
+                        item.setAttachmentList(getAttachments(fav.getMessageId()));
+                    }
+                    System.out.println("FavItem: " + fav);
+                    favs.add(item);
+                }
+                logDuration(LOG, "Favourites Full load", start);
+                displayFavourites(revision, favs);
+            } catch (DbException | FormatException e) {
+                logException(LOG, WARNING, e);
+            }
+        });
+    }
+
+    private void displayFavourites(int revision, List<FavItem> favs) {
+        runOnUiThreadUnlessDestroyed(() -> {
+            if (revision == adapter.getRevision()) {
+                adapter.incrementRevision();
+                if (favs.isEmpty()) list.showData();
+                else adapter.replaceAll(favs);
+            } else {
+                loadFavourites();
+            }
+        });
+    }
+
+    private void displayMessageAttachments(String messageId, List<AttachmentItem> items) {
+        runOnUiThreadUnlessDestroyed(() -> {
+            Pair<Integer, FavItem> pair =
+                    adapter.getMessageItem(messageId);
+            if (pair != null) {
+                ((FavItem) pair.getSecond())
+                        .setAttachmentList(items);
+                adapter.notifyItemChanged(pair.getFirst());
+            }
+        });
+    }
+
+    private void loadMessageAttachments(String messageId) {
+        listener.runOnDbThread(() -> {
+            try {
+                attachmentRetriever.getMessageAttachments(messageId);
+                displayMessageAttachments(messageId, attachmentRetriever.cacheGet(messageId));
+            } catch (DbException e) {
+                logException(LOG, WARNING, e);
+            }
+        });
+    }
+
+    public List<AttachmentItem> getAttachments(String messageId) {
+        List<AttachmentItem> attachments =
+                attachmentRetriever.cacheGet(messageId);
+        if (attachments == null) {
+            loadMessageAttachments(messageId);
+            return emptyList();
+        }
+        return attachments;
+    }
+
 
 }
