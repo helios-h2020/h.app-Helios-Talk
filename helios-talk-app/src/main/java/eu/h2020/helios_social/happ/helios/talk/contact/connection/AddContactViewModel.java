81e9e51bd67c456df1d8ae816a062ec0ba4adf53
vasgat <vasgat@iti.gr>
2021-06-07 15:08:00 +0300

    Update AddContactViewModel -> catch DbExceptions, remove unused code (already in comments)

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/AddContactViewModel.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/AddContactViewModel.java
@@ -11,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+
+import eu.h2020.helios_social.modules.groupcommunications.api.exception.DbException;
 import eu.h2020.helios_social.modules.groupcommunications_utils.db.DatabaseExecutor;
 import eu.h2020.helios_social.modules.groupcommunications_utils.identity.IdentityManager;
 import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.NotNullByDefault;
@@ -26,103 +28,92 @@ import static java.util.logging.Logger.getLogger;
 @NotNullByDefault
 public class AddContactViewModel extends AndroidViewModel {
 
-	private final static Logger LOG =
-			getLogger(AddContactViewModel.class.getName());
+    private final static Logger LOG =
+            getLogger(AddContactViewModel.class.getName());
 
-	private final IdentityManager identityManager;
-	private final ConnectionManager connectionManager;
-	private final PendingContactFactory pendingContactFactory;
-	@DatabaseExecutor
-	private final Executor dbExecutor;
+    private final IdentityManager identityManager;
+    private final ConnectionManager connectionManager;
+    private final PendingContactFactory pendingContactFactory;
+    @DatabaseExecutor
+    private final Executor dbExecutor;
 
-	private final MutableLiveData<String> heliosLink =
-			new MutableLiveData<>();
-	private final MutableLiveEvent<Boolean> remoteLinkEntered =
-			new MutableLiveEvent<>();
-	private final MutableLiveData<LiveResult<Boolean>> addContactResult =
-			new MutableLiveData<>();
-	@Nullable
-	private String remoteHeliosLink;
+    private final MutableLiveData<String> heliosLink =
+            new MutableLiveData<>();
+    private final MutableLiveEvent<Boolean> remoteLinkEntered =
+            new MutableLiveEvent<>();
+    private final MutableLiveData<LiveResult<Boolean>> addContactResult =
+            new MutableLiveData<>();
+    @Nullable
+    private String remoteHeliosLink;
 
-	@Inject
-	AddContactViewModel(Application application,
-			IdentityManager identityManager,
-			ConnectionManager connectionManager,
-			PendingContactFactory pendingContactFactory,
-			@DatabaseExecutor Executor dbExecutor) {
-		super(application);
-		this.identityManager = identityManager;
-		this.connectionManager = connectionManager;
-		this.pendingContactFactory = pendingContactFactory;
-		this.dbExecutor = dbExecutor;
-	}
+    @Inject
+    AddContactViewModel(Application application,
+                        IdentityManager identityManager,
+                        ConnectionManager connectionManager,
+                        PendingContactFactory pendingContactFactory,
+                        @DatabaseExecutor Executor dbExecutor) {
+        super(application);
+        this.identityManager = identityManager;
+        this.connectionManager = connectionManager;
+        this.pendingContactFactory = pendingContactFactory;
+        this.dbExecutor = dbExecutor;
+    }
 
-	void onCreate() {
-		if (heliosLink.getValue() == null) loadHandshakeLink();
-	}
+    void onCreate() {
+        if (heliosLink.getValue() == null) loadHandshakeLink();
+    }
 
-	private void loadHandshakeLink() {
-		dbExecutor.execute(() -> {
-			heliosLink.postValue(identityManager.getHeliosLink());
-		});
-	}
+    private void loadHandshakeLink() {
+        dbExecutor.execute(() -> {
+            heliosLink.postValue(identityManager.getHeliosLink());
+        });
+    }
 
-	LiveData<String> getHeliosLink() {
-		return heliosLink;
-	}
+    LiveData<String> getHeliosLink() {
+        return heliosLink;
+    }
 
-	@Nullable
-	String getRemoteHeliosLink() {
-		return remoteHeliosLink;
-	}
+    @Nullable
+    String getRemoteHeliosLink() {
+        return remoteHeliosLink;
+    }
 
-	void setRemoteHeliosLink(String link) {
-		remoteHeliosLink = link;
-	}
+    void setRemoteHeliosLink(String link) {
+        remoteHeliosLink = link;
+    }
 
-	boolean isValidHeliosLink(@Nullable CharSequence link) {
-		return link != null && LINK_REGEX.matcher(link).find();
-	}
+    boolean isValidHeliosLink(@Nullable CharSequence link) {
+        return link != null && LINK_REGEX.matcher(link).find();
+    }
 
-	LiveEvent<Boolean> getRemoteHeliosLinkEntered() {
-		return remoteLinkEntered;
-	}
+    LiveEvent<Boolean> getRemoteHeliosLinkEntered() {
+        return remoteLinkEntered;
+    }
 
-	void onRemoteLinkEntered() {
-		if (remoteHeliosLink == null) throw new IllegalStateException();
-		remoteLinkEntered.setEvent(true);
-	}
+    void onRemoteLinkEntered() {
+        if (remoteHeliosLink == null) throw new IllegalStateException();
+        remoteLinkEntered.setEvent(true);
+    }
 
-	void addContact(String nickname, String message) {
-		if (remoteHeliosLink == null) throw new IllegalStateException();
-		dbExecutor.execute(() -> {
+    void addContact(String nickname, String message) {
+        if (remoteHeliosLink == null) throw new IllegalStateException();
+        dbExecutor.execute(() -> {
+                               try {
+                                   connectionManager.sendConnectionRequest(pendingContactFactory
+                                                                                   .createOutgoingPendingContact(
+                                                                                           remoteHeliosLink.replace("helios://", ""),
+                                                                                           nickname,
+                                                                                           message));
+                                   addContactResult.postValue(new LiveResult<>(true));
+                               } catch (DbException e) {
+                                   addContactResult.postValue(new LiveResult<>(false));
+                               }
+                           }
+        );
+    }
 
-					connectionManager.sendConnectionRequest(pendingContactFactory
-							.createOutgoingPendingContact(
-									remoteHeliosLink.replace("helios://", ""),
-									nickname,
-									message));
-					addContactResult.postValue(new LiveResult<>(true));
-				}
-		);
-	}
-
-	LiveData<LiveResult<Boolean>> getAddContactResult() {
-		return addContactResult;
-	}
-
-	/*public void updatePendingContact(String name, PendingContact p) {
-		dbExecutor.execute(() -> {
-			try {
-				contactManager.deletePendingContact(p.getId());
-				addContact(name);
-			} catch (NoSuchPendingContactException e) {
-				logException(LOG, WARNING, e);
-			} catch (DbException e) {
-				logException(LOG, WARNING, e);
-				addContactResult.postValue(new LiveResult<>(e));
-			}
-		});
-	}*/
+    LiveData<LiveResult<Boolean>> getAddContactResult() {
+        return addContactResult;
+    }
 
 }
