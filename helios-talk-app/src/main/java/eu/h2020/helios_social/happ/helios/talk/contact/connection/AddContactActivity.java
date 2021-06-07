9e38f37ddcd972a5bb7a7db1c734ad031a0d93f3
vasgat <vasgat@iti.gr>
2021-06-07 15:06:48 +0300

    Update AddContactActivity -> add Logger, remove prefix from url coming from deep link

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/AddContactActivity.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/AddContactActivity.java
@@ -11,6 +11,9 @@ import javax.inject.Inject;
 import androidx.appcompat.app.ActionBar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+
+import java.util.logging.Logger;
+
 import eu.h2020.helios_social.happ.helios.talk.R;
 import eu.h2020.helios_social.happ.helios.talk.activity.ActivityComponent;
 import eu.h2020.helios_social.happ.helios.talk.activity.HeliosTalkActivity;
@@ -26,79 +29,80 @@ import static android.widget.Toast.LENGTH_LONG;
 @MethodsNotNullByDefault
 @ParametersNotNullByDefault
 public class AddContactActivity extends HeliosTalkActivity implements
-		BaseFragmentListener {
+        BaseFragmentListener {
+    private static Logger LOG = Logger.getLogger(AddContactActivity.class.getName());
 
-	@Inject
-	ViewModelProvider.Factory viewModelFactory;
-	private AddContactViewModel viewModel;
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+    private AddContactViewModel viewModel;
 
-	@Override
-	public void injectActivity(ActivityComponent component) {
-		component.inject(this);
-	}
+    @Override
+    public void injectActivity(ActivityComponent component) {
+        component.inject(this);
+    }
 
-	@Override
-	public void onCreate(@Nullable Bundle state) {
-		super.onCreate(state);
-		setContentView(R.layout.activity_fragment_container);
+    @Override
+    public void onCreate(@Nullable Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_fragment_container);
 
-		ActionBar ab = getSupportActionBar();
-		if (ab != null) {
-			ab.setDisplayHomeAsUpEnabled(true);
-		}
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
-		viewModel = ViewModelProviders.of(this, viewModelFactory)
-				.get(AddContactViewModel.class);
-		viewModel.onCreate();
-		viewModel.getRemoteHeliosLinkEntered().observeEvent(this, entered -> {
-			if (entered) {
-				NicknameFragment f = new NicknameFragment();
-				showNextFragment(f);
-			}
-		});
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(AddContactViewModel.class);
+        viewModel.onCreate();
+        viewModel.getRemoteHeliosLinkEntered().observeEvent(this, entered -> {
+            if (entered) {
+                NicknameFragment f = new NicknameFragment();
+                showNextFragment(f);
+            }
+        });
 
-		Intent i = getIntent();
-		if (state == null) {
-			// do not react to the intent again when recreating the activity
-			onNewIntent(i);
-		}
+        Intent i = getIntent();
+        if (state == null) {
+            // do not react to the intent again when recreating the activity
+            onNewIntent(i);
+        }
 
-		if (state == null) {
-			showInitialFragment(new LinkExchangeFragment());
-		}
-	}
+        if (state == null) {
+            showInitialFragment(new LinkExchangeFragment());
+        }
+    }
 
-	@Override
-	protected void onNewIntent(Intent i) {
-		super.onNewIntent(i);
-		String action = i.getAction();
-		if (ACTION_SEND.equals(action) || ACTION_VIEW.equals(action)) {
-			String text = i.getStringExtra(EXTRA_TEXT);
-			String uri = i.getDataString();
-			if (text != null) handleIncomingLink(text);
-			else if (uri != null) handleIncomingLink(uri);
-		}
-	}
+    @Override
+    protected void onNewIntent(Intent i) {
+        super.onNewIntent(i);
+        String action = i.getAction();
+        if (ACTION_SEND.equals(action) || ACTION_VIEW.equals(action)) {
+            String text = i.getStringExtra(EXTRA_TEXT);
+            String uri = "helios://" + i.getDataString().replace("https://helios-social.com/", "");
+            if (text != null) handleIncomingLink(text);
+            else if (uri != null) handleIncomingLink(uri);
+        }
+    }
 
-	private void handleIncomingLink(String link) {
-		if (link.equals(viewModel.getHeliosLink().getValue())) {
-			Toast.makeText(this, R.string.intent_own_link, LENGTH_LONG)
-					.show();
-		} else if (viewModel.isValidHeliosLink(link)) {
-			viewModel.setRemoteHeliosLink(link);
-		} else {
-			Toast.makeText(this, R.string.invalid_link, LENGTH_LONG)
-					.show();
-		}
-	}
+    private void handleIncomingLink(String link) {
+        if (link.equals(viewModel.getHeliosLink().getValue())) {
+            Toast.makeText(this, R.string.intent_own_link, LENGTH_LONG)
+                    .show();
+        } else if (viewModel.isValidHeliosLink(link)) {
+            viewModel.setRemoteHeliosLink(link);
+        } else {
+            Toast.makeText(this, R.string.invalid_link, LENGTH_LONG)
+                    .show();
+        }
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			onBackPressed();
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
