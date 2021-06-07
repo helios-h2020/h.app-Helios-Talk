00f703fcffa6c59af293ab53109ac132ec718948
vasgat <vasgat@iti.gr>
2021-06-07 15:09:20 +0300

    Update LinkExchangeFragment -> define a more explanatory prompt when sharing your helios. link

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/LinkExchangeFragment.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/contact/connection/LinkExchangeFragment.java
@@ -24,6 +24,7 @@ import javax.inject.Inject;
 import androidx.core.app.ShareCompat.IntentBuilder;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+
 import eu.h2020.helios_social.happ.helios.talk.R;
 import eu.h2020.helios_social.happ.helios.talk.activity.ActivityComponent;
 import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.MethodsNotNullByDefault;
@@ -39,152 +40,155 @@ import static java.util.Objects.requireNonNull;
 @MethodsNotNullByDefault
 @ParametersNotNullByDefault
 public class LinkExchangeFragment extends BaseFragment
-		implements OnGlobalLayoutListener {
+        implements OnGlobalLayoutListener {
 
-	private static final String TAG = LinkExchangeFragment.class.getName();
+    private static final String TAG = LinkExchangeFragment.class.getName();
 
-	@Inject
-	ViewModelProvider.Factory viewModelFactory;
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
 
-	private AddContactViewModel viewModel;
+    private AddContactViewModel viewModel;
 
-	private ClipboardManager clipboard;
-	private TextInputLayout linkInputLayout;
-	private TextInputEditText linkInput;
+    private ClipboardManager clipboard;
+    private TextInputLayout linkInputLayout;
+    private TextInputEditText linkInput;
 
-	@Override
-	public String getUniqueTag() {
-		return TAG;
-	}
+    @Override
+    public String getUniqueTag() {
+        return TAG;
+    }
 
-	@Override
-	public void injectFragment(ActivityComponent component) {
-		component.inject(this);
-	}
+    @Override
+    public void injectFragment(ActivityComponent component) {
+        component.inject(this);
+    }
 
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container,
-			@Nullable Bundle savedInstanceState) {
-		if (getActivity() == null || getContext() == null) return null;
+    public LinkExchangeFragment() {
+    }
 
-		viewModel = ViewModelProviders.of(getActivity(), viewModelFactory)
-				.get(AddContactViewModel.class);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        if (getActivity() == null || getContext() == null) return null;
 
-		View v = inflater.inflate(R.layout.fragment_link_exchange,
-				container, false);
+        viewModel = ViewModelProviders.of(getActivity(), viewModelFactory)
+                .get(AddContactViewModel.class);
 
-		linkInputLayout = v.findViewById(R.id.linkInputLayout);
-		linkInput = v.findViewById(R.id.linkInput);
-		if (viewModel.getRemoteHeliosLink() != null) {
-			// This can happen if the link was set via an incoming Intent
-			linkInput.setText(viewModel.getRemoteHeliosLink());
-		}
+        View v = inflater.inflate(R.layout.fragment_link_exchange,
+                                  container, false);
 
-		clipboard = (ClipboardManager) requireContext()
-				.getSystemService(CLIPBOARD_SERVICE);
+        linkInputLayout = v.findViewById(R.id.linkInputLayout);
+        linkInput = v.findViewById(R.id.linkInput);
+        if (viewModel.getRemoteHeliosLink() != null) {
+            // This can happen if the link was set via an incoming Intent
+            linkInput.setText(viewModel.getRemoteHeliosLink());
+        }
 
-		Button pasteButton = v.findViewById(R.id.pasteButton);
-		pasteButton.setOnClickListener(view -> {
-			ClipData clipData = clipboard.getPrimaryClip();
-			if (clipData != null && clipData.getItemCount() > 0)
-				linkInput.setText(clipData.getItemAt(0).getText());
-		});
+        clipboard = (ClipboardManager) requireContext()
+                .getSystemService(CLIPBOARD_SERVICE);
 
-		UiUtils.observeOnce(viewModel.getHeliosLink(), this,
-				this::onHeliosLinkLoaded);
+        Button pasteButton = v.findViewById(R.id.pasteButton);
+        pasteButton.setOnClickListener(view -> {
+            ClipData clipData = clipboard.getPrimaryClip();
+            if (clipData != null && clipData.getItemCount() > 0)
+                linkInput.setText(clipData.getItemAt(0).getText());
+        });
 
-		if (savedInstanceState == null) {
-			ScrollView scrollView = (ScrollView) v;
-			// we need to wait for views to be laid out to get the heights
-			scrollView.getViewTreeObserver().addOnGlobalLayoutListener(this);
-		}
+        UiUtils.observeOnce(viewModel.getHeliosLink(), this,
+                            this::onHeliosLinkLoaded);
 
-		return v;
-	}
+        if (savedInstanceState == null) {
+            ScrollView scrollView = (ScrollView) v;
+            // we need to wait for views to be laid out to get the heights
+            scrollView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        }
 
-	@Override
-	public void onGlobalLayout() {
-		ScrollView scrollView = (ScrollView) requireView();
-		View layout = scrollView.getChildAt(0);
-		int scrollBy = layout.getHeight() - scrollView.getHeight();
-		if (scrollBy > 0) {
-			// smoothScrollTo() is too fast due to the transition animation
-			ObjectAnimator animator = ObjectAnimator
-					.ofInt(scrollView, "scrollY", scrollBy);
-			animator.setDuration(1000);
-			animator.start();
-		}
-		layout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-	}
+        return v;
+    }
 
-	private void onHeliosLinkLoaded(String link) {
-		View v = requireView();
+    @Override
+    public void onGlobalLayout() {
+        ScrollView scrollView = (ScrollView) requireView();
+        View layout = scrollView.getChildAt(0);
+        int scrollBy = layout.getHeight() - scrollView.getHeight();
+        if (scrollBy > 0) {
+            // smoothScrollTo() is too fast due to the transition animation
+            ObjectAnimator animator = ObjectAnimator
+                    .ofInt(scrollView, "scrollY", scrollBy);
+            animator.setDuration(1000);
+            animator.start();
+        }
+        layout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+    }
 
-		TextView linkView = v.findViewById(R.id.linkView);
-		linkView.setText(link);
+    private void onHeliosLinkLoaded(String link) {
+        View v = requireView();
 
-		Button copyButton = v.findViewById(R.id.copyButton);
-		ClipData clip = ClipData.newPlainText(
-				getString(R.string.link_clip_label), link);
-		copyButton.setOnClickListener(view -> {
-			clipboard.setPrimaryClip(clip);
-			Toast.makeText(getContext(), R.string.link_copied_toast,
-					LENGTH_SHORT).show();
-		});
-		copyButton.setEnabled(true);
+        TextView linkView = v.findViewById(R.id.linkView);
+        linkView.setText(link);
 
-		Button shareButton = v.findViewById(R.id.shareButton);
-		shareButton.setOnClickListener(view ->
-				IntentBuilder.from(requireActivity())
-						.setText(link)
-						.setType("text/plain")
-						.startChooser());
-		shareButton.setEnabled(true);
+        Button copyButton = v.findViewById(R.id.copyButton);
+        ClipData clip = ClipData.newPlainText(
+                getString(R.string.link_clip_label), link);
+        copyButton.setOnClickListener(view -> {
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(getContext(), R.string.link_copied_toast,
+                           LENGTH_SHORT).show();
+        });
+        copyButton.setEnabled(true);
 
-		Button continueButton = v.findViewById(R.id.addButton);
-		continueButton.setOnClickListener(view -> onContinueButtonClicked());
-		continueButton.setEnabled(true);
-	}
+        Button shareButton = v.findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(view ->
+                                               IntentBuilder.from(requireActivity())
+                                                       .setText("Let's get connected to helios. TALK! Please add me to your contacts by opening the following link with helios. TALK https://helios-social.com/" + link.replace("helios://", "") + " or by pasting my helios link (" + link + ") directly to the app")
+                                                       .setType("text/plain")
+                                                       .startChooser());
+        shareButton.setEnabled(true);
 
-	/**
-	 * Requires {@link AddContactViewModel#getHeliosLink()} to be loaded.
-	 */
-	@Nullable
-	private String getRemoteHeliosLinkOrNull() {
-		CharSequence link = linkInput.getText();
-		if (link == null || link.length() == 0) {
-			linkInputLayout.setError(getString(R.string.missing_link));
-			linkInput.requestFocus();
-			return null;
-		}
+        Button continueButton = v.findViewById(R.id.addButton);
+        continueButton.setOnClickListener(view -> onContinueButtonClicked());
+        continueButton.setEnabled(true);
+    }
 
-		Matcher matcher = LINK_REGEX.matcher(link);
-		if (matcher.find()) {
-			String linkWithoutSchema = matcher.group(2);
-			// Check also if this is our own link. This was loaded already,
-			// because it enables the Continue button which is the only caller.
-			if (("helios://" + linkWithoutSchema)
-					.equals(viewModel.getHeliosLink().getValue())) {
-				linkInputLayout.setError(getString(R.string.own_link_error));
-				linkInput.requestFocus();
-				return null;
-			}
-			linkInputLayout.setError(null);
-			return link.toString();
-		}
-		linkInputLayout.setError(getString(R.string.invalid_link));
-		linkInput.requestFocus();
-		return null;
-	}
+    /**
+     * Requires {@link AddContactViewModel#getHeliosLink()} to be loaded.
+     */
+    @Nullable
+    private String getRemoteHeliosLinkOrNull() {
+        CharSequence link = linkInput.getText();
+        if (link == null || link.length() == 0) {
+            linkInputLayout.setError(getString(R.string.missing_link));
+            linkInput.requestFocus();
+            return null;
+        }
 
-	private void onContinueButtonClicked() {
-		String link = getRemoteHeliosLinkOrNull();
-		if (link == null) return;  // invalid link
+        Matcher matcher = LINK_REGEX.matcher(link);
+        if (matcher.find()) {
+            String linkWithoutSchema = matcher.group(2);
+            // Check also if this is our own link. This was loaded already,
+            // because it enables the Continue button which is the only caller.
+            if (("helios://" + linkWithoutSchema)
+                    .equals(viewModel.getHeliosLink().getValue())) {
+                linkInputLayout.setError(getString(R.string.own_link_error));
+                linkInput.requestFocus();
+                return null;
+            }
+            linkInputLayout.setError(null);
+            return link.toString();
+        }
+        linkInputLayout.setError(getString(R.string.invalid_link));
+        linkInput.requestFocus();
+        return null;
+    }
 
-		viewModel.setRemoteHeliosLink(link);
-		viewModel.onRemoteLinkEntered();
-	}
+    private void onContinueButtonClicked() {
+        String link = getRemoteHeliosLinkOrNull();
+        if (link == null) return;  // invalid link
+
+        viewModel.setRemoteHeliosLink(link);
+        viewModel.onRemoteLinkEntered();
+    }
 
 }
