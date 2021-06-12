a3388258d4833b0d496dc149fa7830c770587c37
vasgat <vasgat@iti.gr>
2021-06-12 22:59:02 +0300

    update TextAttachmentController to handle except from image attachments also file attachments

--- helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/view/TextAttachmentController.java
+++ helios-talk-app/src/main/java/eu/h2020/helios_social/happ/helios/talk/view/TextAttachmentController.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.UiThread;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.customview.view.AbsSavedState;
 import androidx.lifecycle.LifecycleOwner;
 
@@ -39,24 +40,30 @@ import eu.h2020.helios_social.happ.helios.talk.R;
 import eu.h2020.helios_social.happ.helios.talk.android.util.AndroidUtils;
 import eu.h2020.helios_social.happ.helios.talk.attachment.AttachmentItem;
 import eu.h2020.helios_social.happ.helios.talk.conversation.ConversationViewModel;
+import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Message;
 import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.NotNullByDefault;
 
 @UiThread
 @NotNullByDefault
-public class TextAttachmentController extends TextSendController implements ImagePreview.ImagePreviewListener {
+public class TextAttachmentController extends TextSendController implements ImagePreview.ImagePreviewListener, FileAttachmentPreview.FileAttachmentPreviewListener {
 
     private final Context ctx;
     private final ImagePreview imagePreview;
+    private final FileAttachmentPreview fileAttachmentPreview;
     private final AttachmentListener attachmentListener;
     private final CompositeSendButton sendButton;
+    private final AppCompatImageButton fileAttachmentButton;
+    private final AppCompatImageButton capturePhotoButton;
 
-    private final List<Uri> imageUris = new ArrayList<>();
+    private final List<Uri> uris = new ArrayList<>();
     private final List<AttachmentItem> attachments = new ArrayList();
     private final CharSequence textHint;
     private boolean loadingUris = false;
+    private Message.Type messageType = Message.Type.TEXT;
 
     public TextAttachmentController(Context ctx, TextInputView v,
                                     ImagePreview imagePreview,
+                                    FileAttachmentPreview fileAttachmentPreview,
                                     AttachmentListener attachmentListener) {
         super(v, attachmentListener, false);
 
@@ -65,7 +72,13 @@ public class TextAttachmentController extends TextSendController implements Imag
         this.imagePreview = imagePreview;
         this.imagePreview.setImagePreviewListener(this);
 
+        this.fileAttachmentPreview = fileAttachmentPreview;
+        this.fileAttachmentPreview.setFileAttachmentPreviewListener(this);
+
         sendButton = (CompositeSendButton) compositeSendButton;
+        fileAttachmentButton = v.findViewById(R.id.attachFileButton);
+        fileAttachmentButton.setOnClickListener(l -> onFileAttachmentButtonClicked());
+        capturePhotoButton = v.findViewById(R.id.captureFromCameraButton);
         sendButton.setOnImageClickListener(view -> onImageButtonClicked());
 
         textHint = textInput.getHint();
@@ -78,12 +91,21 @@ public class TextAttachmentController extends TextSendController implements Imag
                 (!textIsEmpty || canSendEmptyText());
         if (loadingUris) {
             sendButton.showProgress(true);
-        } else if (imageUris.isEmpty()) {
+        } else if (uris.isEmpty()) {
             sendButton.showProgress(false);
             sendButton.showImageButton(textIsEmpty, sendEnabled);
+            if (textIsEmpty) {
+                fileAttachmentButton.setVisibility(View.VISIBLE);
+                capturePhotoButton.setVisibility(View.VISIBLE);
+            } else {
+                fileAttachmentButton.setVisibility(View.GONE);
+                capturePhotoButton.setVisibility(View.GONE);
+            }
         } else {
             sendButton.showProgress(false);
             sendButton.showImageButton(false, sendEnabled);
+            fileAttachmentButton.setVisibility(View.GONE);
+            capturePhotoButton.setVisibility(View.GONE);
         }
     }
 
@@ -91,14 +113,14 @@ public class TextAttachmentController extends TextSendController implements Imag
     public void onSendEvent() {
         if (canSend()) {
             if (loadingUris) throw new AssertionError();
-            listener.onSendClick(textInput.getText(), attachments);
+            listener.onSendClick(textInput.getText(), attachments, messageType);
             reset();
         }
     }
 
     @Override
     protected boolean canSendEmptyText() {
-        return !imageUris.isEmpty();
+        return !uris.isEmpty();
     }
 
     public void setImagesSupported() {
@@ -106,15 +128,22 @@ public class TextAttachmentController extends TextSendController implements Imag
     }
 
     private void onImageButtonClicked() {
-        Intent intent = getAttachFileIntent();
+        Intent intent = getAttachImagesIntent();
         if (attachmentListener.getLifecycle().getCurrentState() != DESTROYED) {
             attachmentListener.onAttachImage(intent);
         }
     }
 
-    private Intent getAttachFileIntent() {
+    private void onFileAttachmentButtonClicked() {
+        Intent intent = getAttachFileIntent();
+        if (attachmentListener.getLifecycle().getCurrentState() != DESTROYED) {
+            attachmentListener.onAttachFile(intent);
+        }
+    }
+
+    private Intent getAttachImagesIntent() {
         Intent intent = new Intent(SDK_INT >= 19 ?
-                ACTION_OPEN_DOCUMENT : ACTION_GET_CONTENT);
+                                           ACTION_OPEN_DOCUMENT : ACTION_GET_CONTENT);
         intent.setType("image/*");
         intent.addCategory(CATEGORY_OPENABLE);
         if (SDK_INT >= 19)
@@ -123,41 +152,66 @@ public class TextAttachmentController extends TextSendController implements Imag
         return intent;
     }
 
-    public void onImageReceived(@Nullable Intent resultData) {
+    private Intent getAttachFileIntent() {
+        Intent intent = new Intent(SDK_INT >= 19 ?
+                                           ACTION_OPEN_DOCUMENT : ACTION_GET_CONTENT);
+        String[] mimeTypes = {"text/*", "video/*", "application/pdf"};
+        intent.setType("*/*");
+        intent.addCategory(CATEGORY_OPENABLE);
+        if (SDK_INT >= 19)
+            intent.putExtra(EXTRA_MIME_TYPES, mimeTypes);
+        if (SDK_INT >= 18) intent.putExtra(EXTRA_ALLOW_MULTIPLE, false);
+        return intent;
+    }
+
+    public void onAttachmentsReceived(@Nullable Intent resultData, Message.Type messageType) {
+        this.messageType = messageType;
         if (resultData == null) return;
-        if (loadingUris || !imageUris.isEmpty()) throw new AssertionError();
+        if (loadingUris || !uris.isEmpty()) throw new AssertionError();
         List<Uri> newUris = new ArrayList<>();
         if (resultData.getData() != null) {
             newUris.add(resultData.getData());
-            onNewUris(false, newUris);
+            onNewUris(false, newUris, messageType);
         } else if (SDK_INT >= 18 && resultData.getClipData() != null) {
             ClipData clipData = resultData.getClipData();
             for (int i = 0; i < clipData.getItemCount(); i++) {
                 newUris.add(clipData.getItemAt(i).getUri());
             }
-            onNewUris(false, newUris);
+            onNewUris(false, newUris, messageType);
         }
     }
 
-    private void onNewUris(boolean restart, List<Uri> newUris) {
+    private void onNewUris(boolean restart, List<Uri> newUris, Message.Type messageType) {
         if (newUris.isEmpty()) return;
         if (loadingUris) throw new AssertionError();
         loadingUris = true;
-        if (newUris.size() > MAX_IMAGE_ATTACHMENTS_PER_MESSAGE) {
-            newUris = newUris.subList(0, MAX_IMAGE_ATTACHMENTS_PER_MESSAGE);
-            attachmentListener.onTooManyAttachments();
-        }
-        imageUris.addAll(newUris);
-        updateViewState();
-        textInput.setHint(R.string.image_caption_hint);
-        List<ImagePreviewItem> items = ImagePreviewItem.fromUris(imageUris);
-        imagePreview.showPreview(items);
+        if (messageType == Message.Type.IMAGES) {
+            if (newUris.size() > MAX_IMAGE_ATTACHMENTS_PER_MESSAGE) {
+                newUris = newUris.subList(0, MAX_IMAGE_ATTACHMENTS_PER_MESSAGE);
+                attachmentListener.onTooManyImageAttachments();
+            }
+            uris.addAll(newUris);
+            updateViewState();
+            textInput.setHint(R.string.image_caption_hint);
 
-        for (Uri imageUri : imageUris) {
-            AttachmentItem imageAttachmentItem = new AttachmentItem(imageUri, ctx.getContentResolver().getType(imageUri));
-            attachments.add(imageAttachmentItem);
-            imagePreview.loadPreviewImage(imageAttachmentItem);
+            List<ImagePreviewItem> items = ImagePreviewItem.fromUris(uris);
+            imagePreview.showPreview(items);
 
+            for (Uri imageUri : uris) {
+                AttachmentItem imageAttachmentItem = new AttachmentItem(imageUri, ctx.getContentResolver().getType(imageUri));
+                attachments.add(imageAttachmentItem);
+                imagePreview.loadPreviewImage(imageAttachmentItem);
+
+            }
+        } else if (messageType == Message.Type.FILE_ATTACHMENT) {
+            uris.addAll(newUris);
+            updateViewState();
+            textInput.setHint(R.string.image_caption_hint);
+
+            Uri fileUri = uris.get(0);
+            AttachmentItem attachmentItem = new AttachmentItem(fileUri, ctx.getContentResolver().getType(fileUri));
+            attachments.add(attachmentItem);
+            fileAttachmentPreview.showPreview(new FileAttachmentPreviewItem(fileUri));
         }
         loadingUris = false;
     }
@@ -167,10 +221,14 @@ public class TextAttachmentController extends TextSendController implements Imag
         textInput.setHint(textHint);
         // hide image layout
         imagePreview.setVisibility(View.GONE);
-        // reset image URIs
-        imageUris.clear();
+        // hide file attachment layout
+        fileAttachmentPreview.setVisibility(View.GONE);
+        // reset attachment URIs
+        uris.clear();
         //reset attachments
         attachments.clear();
+        //reset message type as text
+        messageType = Message.Type.TEXT;
         // definitely not loading anymore
         loadingUris = false;
         // show the image button again, so images can get attached
@@ -181,7 +239,8 @@ public class TextAttachmentController extends TextSendController implements Imag
     public Parcelable onSaveInstanceState(@Nullable Parcelable superState) {
         SavedState state =
                 new SavedState(superState == null ? EMPTY_STATE : superState);
-        state.imageUris = imageUris;
+        state.uris = uris;
+        state.messageType = messageType;
         return state;
     }
 
@@ -189,8 +248,8 @@ public class TextAttachmentController extends TextSendController implements Imag
     @Nullable
     public Parcelable onRestoreInstanceState(Parcelable inState) {
         SavedState state = (SavedState) inState;
-        if (!imageUris.isEmpty()) throw new AssertionError();
-        if (state.imageUris != null) onNewUris(true, state.imageUris);
+        if (!uris.isEmpty()) throw new AssertionError();
+        if (state.uris != null) onNewUris(true, state.uris, state.messageType);
         return state.getSuperState();
     }
 
@@ -215,7 +274,8 @@ public class TextAttachmentController extends TextSendController implements Imag
     private static class SavedState extends AbsSavedState {
 
         @Nullable
-        private List<Uri> imageUris;
+        private List<Uri> uris;
+        private Message.Type messageType;
 
         private SavedState(Parcelable superState) {
             super(superState);
@@ -224,13 +284,13 @@ public class TextAttachmentController extends TextSendController implements Imag
         private SavedState(Parcel in) {
             super(in);
             //noinspection unchecked
-            imageUris = in.readArrayList(Uri.class.getClassLoader());
+            uris = in.readArrayList(Uri.class.getClassLoader());
         }
 
         @Override
         public void writeToParcel(Parcel out, int flags) {
             super.writeToParcel(out, flags);
-            out.writeList(imageUris);
+            out.writeList(uris);
         }
 
         public static final Creator<SavedState> CREATOR =
@@ -252,6 +312,8 @@ public class TextAttachmentController extends TextSendController implements Imag
 
         void onAttachImage(Intent intent);
 
-        void onTooManyAttachments();
+        void onAttachFile(Intent intent);
+
+        void onTooManyImageAttachments();
     }
 }
